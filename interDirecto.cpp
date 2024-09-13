c910da6340f9deb73742b9dbe1c0a6b314664faa
Dante AT <dalvarezt@unjbg.edu.pe>
2024-09-13 14:58:08 -0500

    Se agrego la función interDirDer

--- interDirecto.cpp
+++ interDirecto.cpp
@@ -3,13 +3,20 @@ using namespace std;
 
 void ingresarNumArreglo(int, int[]);
 void mostrarArreglo(int, int[]);
+void interDirDer(int, int[]);
 
 int main() {
 	int numerosArreglo, arreglo[100];
 	cout << "Ingresa cuantos numeros tendra el arreglo (MAX 100): ";
 	cin >> numerosArreglo;
-	ingresarNumArreglo(numerosArreglo, arreglo);
-	mostrarArreglo(numerosArreglo, arreglo);
+	if ( numerosArreglo > 100 ) {
+		cout << "ERROR: SOBREPASO EL LIMITE" << endl;
+	} else {
+		ingresarNumArreglo(numerosArreglo, arreglo);
+		mostrarArreglo(numerosArreglo, arreglo);
+		interDirDer(numerosArreglo, arreglo);
+		mostrarArreglo(numerosArreglo, arreglo);
+	}
 }
 
 void ingresarNumArreglo(int numerosArreglo, int arreglo[100]) {
@@ -23,5 +30,19 @@ void mostrarArreglo(int numerosArreglo, int arreglo[100]) {
 	cout << "Arreglo ingresado: " << endl;
 	for ( int i = 0; i < numerosArreglo; i++ ) {
 		cout << arreglo[i] << "  ";
+	}
+	cout << endl;
+}
+
+void interDirDer(int numerosArreglo, int arreglo[100]) {
+	int aux;
+	for ( int i = 0; i < numerosArreglo-1; i++ ) {
+		for ( int j = 0; j < numerosArreglo-i-1; j++ ) {
+			if ( arreglo[j] > arreglo [j+1] ) {
+				aux = arreglo[j];
+				arreglo[j] = arreglo[j+1];
+				arreglo[j+1] = aux;
+			}
+		}
 	}
 }
