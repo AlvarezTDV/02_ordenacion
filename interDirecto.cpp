6bb2c1dc8471470c962e16ecce7923f63d5c422b
Dante <dalvarezt@unjbg.edu.pe>
2024-09-14 14:11:32 -0500

    Se añadieron comentarios para comprender mejor el código

--- interDirecto.cpp
+++ interDirecto.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+//INICIALIZACION DE LAS FUNCIONES
 void ingresarNumArreglo(int, int[]);
 void mostrarArreglo(int, int[]);
 void interDirDer(int, int[]);
@@ -9,6 +10,7 @@ int main() {
 	int numerosArreglo, arreglo[100];
 	cout << "Ingresa cuantos numeros tendra el arreglo (MAX 100): ";
 	cin >> numerosArreglo;
+	//CONDICION PARA COMPROBAR SI EL USUARIO NO SOBREPASO EL LIMITE
 	if ( numerosArreglo > 100 ) {
 		cout << "ERROR: SOBREPASO EL LIMITE" << endl;
 	} else {
@@ -19,6 +21,7 @@ int main() {
 	}
 }
 
+//FUNCION PARA INGRESAR DATOS A UN ARREGLO
 void ingresarNumArreglo(int numerosArreglo, int arreglo[100]) {
 	for ( int i = 0; i < numerosArreglo; i++ ) {
 		cout << "Ingresa el valor nro " << i+1 << " :";
@@ -26,6 +29,7 @@ void ingresarNumArreglo(int numerosArreglo, int arreglo[100]) {
 	}
 }
 
+//FUNCION PARA MOSTRAR EL ARREGLO
 void mostrarArreglo(int numerosArreglo, int arreglo[100]) {
 	cout << "Arreglo ingresado: " << endl;
 	for ( int i = 0; i < numerosArreglo; i++ ) {
@@ -34,6 +38,7 @@ void mostrarArreglo(int numerosArreglo, int arreglo[100]) {
 	cout << endl;
 }
 
+//FUNCION INTERCAMBIO DIRECTO: POR LA DERECHA
 void interDirDer(int numerosArreglo, int arreglo[100]) {
 	int aux;
 	for ( int i = 0; i < numerosArreglo-1; i++ ) {
