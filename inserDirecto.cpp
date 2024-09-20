40b513ca2ea1c506a9a82e55f99d45987d5448b2
Dante AT <dalvarezt@unjbg.edu.pe>
2024-09-20 15:42:33 -0500

    Se añadió el ordenamiento: inserción binaria
    Método de ordenación

--- inserDirecto.cpp
+++ inserDirecto.cpp
@@ -5,6 +5,8 @@ void ingresarNumArreglo(int, int[], int[]);
 void mostrarArregloCop(int, int[]);
 void mostrarArregloOri(int, int[]);
 void insercionDir(int, int[]);
+void insercionBin(int, int[]);
+void reseteo(int, int[], int[]);
 
 int main() {
 	int capacidad, arreglo[100], arreglo1[100], seleccion;
@@ -27,15 +29,20 @@ int main() {
 			cin >> seleccion;
 			switch ( seleccion ) {
 				case 1:
+					reseteo(capacidad, arreglo1, arreglo);
 					cout << "ARREGLO ORDENADO CON INSERSION DIRECTA: " << endl;
-					insercionDir(capacidad, arreglo);
-					mostrarArregloCop(capacidad, arreglo);
+					insercionDir(capacidad, arreglo1);
+					mostrarArregloCop(capacidad, arreglo1);
 					break;
 				case 2:
+					reseteo(capacidad, arreglo1, arreglo);
+					cout << "ARREGLO ORDENADO CON INSERCION BINARIA: " << endl;
+					insercionBin(capacidad, arreglo1);
+					mostrarArregloCop(capacidad, arreglo1);
 					break;
 				case 3:
 					cout << endl << "ARREGLO: " << endl;
-					mostrarArregloOri(capacidad, arreglo1);
+					mostrarArregloOri(capacidad, arreglo);
 					break;
 				case 4:
 					cout << "SALIENDO DEL PROGRAMA ..." << endl;
@@ -50,7 +57,7 @@ int main() {
 }
 
 //FUNCION PARA INGRESAR DATOS A UN ARREGLO
-void ingresarNumArreglo(int capacidad, int arreglo[100], int arreglo1[100]) {
+void ingresarNumArreglo( int capacidad, int arreglo[100], int arreglo1[100] ) {
 	for ( int i = 0; i < capacidad; i++ ) {
 		cout << "Ingresa el valor nro " << i+1 << ": ";
 		cin >> arreglo[i];
@@ -59,8 +66,15 @@ void ingresarNumArreglo(int capacidad, int arreglo[100], int arreglo1[100]) {
 	}
 }
 
+//FUNCION PARA RESETEAR LOS DATOS DEL ARREGLO COPIA
+void reseteo( int capacidad, int arreglo1[100], int arreglo[100] ) {
+	for ( int i = 0; i < capacidad; i++ ) {
+		arreglo1[i] = arreglo[i];
+	}
+}
+
 //FUNCION PARA MOSTRAR EL ARREGLO ORIGINAL
-void mostrarArregloOri(int capacidad, int arreglo[100]) {
+void mostrarArregloOri( int capacidad, int arreglo[100] ) {
 	for ( int i = 0; i < capacidad; i++ ) {
 		cout << arreglo[i] << "  ";
 	}
@@ -68,7 +82,7 @@ void mostrarArregloOri(int capacidad, int arreglo[100]) {
 }
 
 //FUNCION PARA MOSTRAR EL ARREGLO COPIADO
-void mostrarArregloCop(int capacidad, int arreglo1[100]) {
+void mostrarArregloCop( int capacidad, int arreglo1[100] ) {
 	for ( int i = 0; i < capacidad; i++ ) {
 		cout << arreglo1[i] << "  ";
 	}
@@ -76,15 +90,39 @@ void mostrarArregloCop(int capacidad, int arreglo1[100]) {
 }
 
 //FUNCION INSERCION: INSERCION DIRECTA
-void insercionDir( int capacidad, int arreglo[100]) {
+void insercionDir( int capacidad, int arreglo1[100] ) {
 	int aux, k;
 	for ( int i = 1; i < capacidad; i++ ) {
-		aux = arreglo[i];
+		aux = arreglo1[i];
 		k = i - 1;
-		while ( k >= 0 && aux < arreglo[k] ) {
-			arreglo[k+1] = arreglo[k];
+		while ( k >= 0 && aux < arreglo1[k] ) {
+			arreglo1[k+1] = arreglo1[k];
 			k = k - 1;
 		}
-		arreglo[k+1] = aux;
+		arreglo1[k+1] = aux;
+	}
+}
+
+//FUNCION INSERCION: INSERCION BINARIA
+void insercionBin( int capacidad, int arreglo1[100] ) {
+	int aux, izq, der, m;
+	for ( int i = 1; i < capacidad; i++ ) {
+		aux = arreglo1[i];
+		izq = 0;
+		der = i - 1;
+		while ( izq <= der ) {
+			m = int((izq + der) / 2);
+			if ( aux < arreglo1[m] ) {
+				der = m - 1;
+			} else {
+				izq = m + 1;
+			}
+		}
+		int j = i - 1;
+		while ( j >= izq ) {
+			arreglo1[j+1] = arreglo1[j];
+			j = j - 1;
+		}
+		arreglo1[izq] = aux;
 	}
 }
